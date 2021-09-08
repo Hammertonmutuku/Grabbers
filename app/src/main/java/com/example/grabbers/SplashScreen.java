2aba16a6952f6f3868c7c5bffada30499fdaa045
MutukuHammerton <hammertonmutuku@gmail.com>
2021-09-08 10:54:41 +0300

    worked on the splash screen and its animation

---
+++ app/src/main/java/com/example/grabbers/SplashScreen.java
@@ -0,0 +1,37 @@
+package com.example.grabbers;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class SplashScreen extends AppCompatActivity {
+
+    //Variables
+    Animation topAnim, bottomAnim;
+    ImageView rider;
+    TextView welcome;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash);
+
+        //Animations
+        topAnim = AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
+
+        //Hooks
+        rider = findViewById(R.id.rider1);
+        welcome = findViewById(R.id.grabbers1);
+
+        rider.setAnimation(topAnim);
+        welcome.setAnimation(bottomAnim);
+    }
+}
