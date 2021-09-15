3711143018786701c7440fb371452cc8f701df3f
MutukuHammerton <hammertonmutuku@gmail.com>
2021-09-15 09:30:25 +0300

    made validation to register and login

--- app/src/main/java/com/example/grabbers/Login.java
+++ app/src/main/java/com/example/grabbers/Login.java
@@ -2,14 +2,80 @@ package com.example.grabbers;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 public class Login extends AppCompatActivity {
 
+    TextView  signup1, forgotpassword;
+    TextInputLayout username, password;
+    Button signin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+//        hooks
+        signup1 = findViewById(R.id.signup1);
+        forgotpassword = findViewById(R.id.forgot);
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        signin =  findViewById(R.id.signin2);
+
+
+        signup1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Login.this,Signup.class);
+                startActivity(intent);
+            }
+        });
     }
+
+    public Boolean validateUsername() {
+        String val = username.getEditText().getText().toString();
+
+        if(val.isEmpty()) {
+            username.setError("Username cannot be empty");
+            return  false;
+        }else  {
+            username.setError("null");
+            username.setErrorEnabled(false);
+            return  true;
+        }
+
+    }
+
+    public Boolean validatePassword() {
+        String val = password.getEditText().getText().toString();
+
+        if(val.isEmpty()) {
+            password.setError("Password cannot be empty");
+            return  false;
+        }else  {
+            password.setError("null");
+            password.setErrorEnabled(false);
+            return  true;
+        }
+
+    }
+
+    public void loginUser(View view) {
+
+        if (!validateUsername() || !validatePassword()) {
+            return;
+        }
+        String username1 = username.getEditText().getText().toString();
+        String password1 = password.getEditText().getText().toString();
+    }
+
+
 }
--- app/src/main/java/com/example/grabbers/Signup.java
+++ app/src/main/java/com/example/grabbers/Signup.java
@@ -3,12 +3,135 @@ package com.example.grabbers;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 public class Signup extends AppCompatActivity {
 
+    //variables
+    TextInputLayout usernameS, emailS, phoneS, passwordS, confirmsS;
+    Button signup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        //Hooks
+        usernameS =findViewById(R.id.usernameA);
+        emailS = findViewById(R.id.emailA);
+        phoneS = findViewById(R.id.phoneA);
+        passwordS =findViewById(R.id.passwordA);
+        confirmsS = findViewById(R.id.confrim_passwordA);
+        signup = findViewById(R.id.signupA);
+
+    }
+
+    private Boolean validateName() {
+        String val =  usernameS.getEditText().getText().toString();
+        String noWhiteSpace = "()?=\\s+$";
+
+        if(val.isEmpty()) {
+            usernameS.setError("Field cannot be empty");
+            return  false;
+        }
+        else if(val.length()>=15) {
+            usernameS.setError(("username is too long"));
+            return false;
+        }
+        else if(val.matches(noWhiteSpace)) {
+            usernameS.setError("Field cannot allow white space");
+            return false;
+        }else {
+            usernameS.setError(null);
+            usernameS.setErrorEnabled(false);
+            return true;
+        }
+    }
+
+    private Boolean validateEmail() {
+        String val =  emailS.getEditText().getText().toString();
+        String emailPattern = "[a-zA-X0-9._-]+@[A-Z]+\\.+[A-Z]+";
+
+
+        if(val.isEmpty()) {
+            emailS.setError("Field cannot be empty");
+            return  false;
+        }else if(!val.matches(emailPattern)) {
+            emailS.setError("Invalid email");
+            return false;
+        }
+        else {
+            emailS.setError(null);
+            emailS.setErrorEnabled(false);
+            return true;
+        }
+    }
+    private Boolean validatePhone() {
+        String val =  phoneS.getEditText().getText().toString();
+
+        if(val.isEmpty()) {
+            phoneS.setError("Field cannot be empty");
+            return  false;
+        } else {
+            phoneS.setError(null);
+            return true;
+        }
+    }
+    private Boolean validatePassword() {
+        String val =  passwordS.getEditText().getText().toString();
+        String passwordval = "^" +
+                //"(?=.*[0-9) " +    //at least 1 digit
+                //"(?=.*[a-z]" +     //at least 1 uppercase
+                //"(?=.[A-Z]"  +         //at least 1 lowercase
+                "(?=.*[a-zA-Z])" +        //any letter
+                "(?=.*[@#$%^&+=])" +    //  //at least 1 special character
+                "(?=\\S+$)" +         //nowhitespace
+                ".{4,}" +                 //at least 4 characters
+                "$";
+
+        if(val.isEmpty()) {
+            passwordS.setError("Field cannot be empty");
+            return  false;
+        }else if(!val.matches(passwordval)) {
+            passwordS.setError("Paasword is weak");
+            return  false;
+        }
+        else{
+            passwordS.setError(null);
+            confirmsS.setErrorEnabled(true);
+            return true;
+        }
+    }
+
+    private Boolean validateConfrimPassword() {
+        String val =  passwordS.getEditText().getText().toString();
+        String con = confirmsS.getEditText().getText().toString();
+
+        if(con.isEmpty()) {
+            confirmsS.setError("Field Cannot be empty");
+            return false;
+        }else if (!con.matches(val)) {
+            confirmsS.setError("Does not match password");
+            return  false;
+        }else {
+            confirmsS.setError(null);
+            confirmsS.setErrorEnabled(true);
+            return true;
+        }
+    }
+
+    public void registerUser (View view) {
+
+        if (!validateName() | !validateEmail() | !validatePhone() | !validatePassword() | !validateConfrimPassword()) {
+            return;
+        }
+        String username2 =  usernameS.getEditText().getText().toString();
+        String password2 =  passwordS.getEditText().getText().toString();
+        String email2 = emailS.getEditText().getText().toString();
+        String phone2 = phoneS.getEditText().getText().toString();
+        String confirm2 = confirmsS.getEditText().getText().toString();
+
     }
 }
