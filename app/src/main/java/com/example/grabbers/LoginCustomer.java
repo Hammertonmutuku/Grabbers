9e81fbac9997ab7595f3a665d051c8629d861ffb
MutukuHammerton <hammertonmutuku@gmail.com>
2021-09-17 21:04:50 +0300

    created the login activity for a rider

--- app/src/main/java/com/example/grabbers/LoginCustomer.java
+++ app/src/main/java/com/example/grabbers/LoginCustomer.java
@@ -7,9 +7,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.database.DataSnapshot;
@@ -19,7 +17,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
-public class Login extends AppCompatActivity {
+public class LoginCustomer extends AppCompatActivity {
 
     TextView  signup1, forgotpassword;
     TextInputLayout username, password;
@@ -41,7 +39,7 @@ public class Login extends AppCompatActivity {
         signup1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Login.this,Signup.class);
+                Intent intent = new Intent(LoginCustomer.this, Signup.class);
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/example/grabbers/MainActivity.java
+++ app/src/main/java/com/example/grabbers/MainActivity.java
@@ -21,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
         logrider.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent =  new Intent(MainActivity.this,Login.class);
+                Intent intent =  new Intent(MainActivity.this, LoginCustomer.class);
                 startActivity(intent);
             }
         });
